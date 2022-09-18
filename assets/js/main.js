12dcdd5d6e17812eacdefbf8a8b977785ab9092f
sanacostato <saacosta.dwfsc9@ucamp.utel.edu.mx>
2022-09-18 13:42:17 -0500

    fin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,39 @@
 console.log("Entro al main.js");
+
+const imgRandom = document.getElementById('imgRandom');
+const URL = "https://picsum.photos/200/300";
+
+//podria ser lo mismo de function (randonimage)
+const randomImage = () =>{
+    console.log("entro a random image");
+    
+    
+    fetch(URL)
+   // .then((response) => { 
+  // return response.json();// en esta no necesitamos convertir a JSON por la API
+//  })
+    .then((data) =>{
+     console.log(data);
+     imgRandom.src = data.url;
+  })
+    .catch((error) => {
+    console.log("Ocurrio un error");
+  });
+    
+
+//     fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/mxn.json')
+//   .then((response) => { 
+//  return response.json();// para convertir a JSON
+// })
+//   .then((data) =>{
+//    console.log(data);
+// })
+//   .catch((error) => {
+//   console.log("Ocurrio un error");
+// });
+  
+  // esto es lo mismo de arriba
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+}
